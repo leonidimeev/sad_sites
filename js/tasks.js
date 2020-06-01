be0aee438b4fb24900d075ae4b02684bd7535340
Leonid Imeev <leonidimeev@yandex.ru>
2020-06-02 00:30:46 +0900

    Finally end mb

--- js/tasks.js
+++ js/tasks.js
@@ -70,24 +70,24 @@ function movebackopacityandwritemsg(){
 	stoptimer();
 }
 
-function checkhisemail(){
-	flag = false;
-	for (i=0; i<document.check_user_form.email_input_box.length; i++){
-		alert('a')
-		if (document.check_user_form.email_input_box[i].value.equals('@')){
-			flag = true;
-			alert('s');
-		}
-	}
-	alert(flag);
+function ValidMail() {
+    var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+    var myMail = document.getElementById('email').value;
+    var valid = re.test(myMail);
+    if (valid) output = 'Адрес эл. почты введен правильно!';
+    else output = 'Адрес электронной почты введен неправильно!';
+    document.getElementById('message').innerHTML = output;
+    return valid;
 }
-
-
-
-
-
-
-
-
+ 
+function ValidPhone() {
+    var re = /^\d[\d\(\)\ -]{4,14}\d$/;
+    var myPhone = document.getElementById('phone').value;
+    var valid = re.test(myPhone);
+    if (valid) output = 'Номер телефона введен правильно!';
+    else output = 'Номер телефона введен неправильно!';
+    document.getElementById('message').innerHTML = document.getElementById('message').innerHTML+'<br />'+output;
+    return valid;
+}  
 
 
---
+++ jsraspisanie.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+$(document).ready(function(){
+  $('a').on('click', function(e){
+    e.preventDefault();
+  });
+    
+  $('#ddmenu li').hover(function () {
+     clearTimeout($.data(this,'timer'));
+     $('ul',this).stop(true,true).slideDown(200);
+  }, function () {
+    $.data(this,'timer', setTimeout($.proxy(function() {
+      $('ul',this).stop(true,true).slideUp(200);
+    }, this), 100));
+  });
+
+});
+</script>
